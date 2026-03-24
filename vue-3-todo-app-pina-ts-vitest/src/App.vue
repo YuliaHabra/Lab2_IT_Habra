d48fe36f63de28b0d5e375165774f9b6722298d5
Yulia <yuliia.habra.pp.2023@lpnu.ua>
2026-03-25 00:19:04 +0200

    Lab6 Done exactly

--- vue-3-todo-app-pina-ts-vitest/src/App.vue
+++ vue-3-todo-app-pina-ts-vitest/src/App.vue
@@ -2,11 +2,23 @@
 import TodoApp from './components/TodoApp.vue'
 document.title = 'To-Do App with Vue 3, Pina w/ Local Storage, Vitest and Playwright'
 const appStatus = import.meta.env.VITE_APP_STATUS
+// Функція для симуляції помилки для Sentry [cite: 130]
+const throwError = () => {
+  throw new Error("Sentry Test Error: Something went wrong in Lab 6!"); 
+};
 </script>
 
 <template>
   <TodoApp />
   <div class="env-status">
     Поточний режим: <strong>{{ appStatus }}</strong>
+
+    <button 
+      @click="throwError" 
+      style="background-color: #e74c3c; color: white; padding: 10px 20px; border: none; border-radius: 5px; cursor: pointer; font-weight: bold;"
+    >
+      Break the world (Sentry Test)
+    </button>
+
   </div>
 </template>
--- vue-3-todo-app-pina-ts-vitest/src/main.ts
+++ vue-3-todo-app-pina-ts-vitest/src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import posthog from 'posthog-js' // Імпортуємо PostHog [cite: 494]
+import * as Sentry from "@sentry/vue"; // Імпортуємо SDK Sentry [cite: 75]
 import App from './App.vue'
 
 import './index.css'
@@ -13,6 +14,34 @@ posthog.init('phc_d3iSi4oqvlJplx9crZyuHDeoxWgefz7WsNI8j8erO45', {
 })
 
 const app = createApp(App)
+
+// Додаємо ініціалізацію Sentry [cite: 76, 112]
+Sentry.init({
+  app,
+  // Твій персональний DSN ключ [cite: 71, 79]
+  dsn: "https://00a15e4bb3de8649d48e0a89b6a05b70@o4511101256335360.ingest.de.sentry.io/4511101292445776",
+  
+  integrations: [
+    Sentry.browserTracingIntegration(), // Відстеження продуктивності [cite: 82, 345]
+    Sentry.replayIntegration(),        // Запис сесій користувача [cite: 83]
+  ],
+
+  // Tracing: Записувати 100% транзакцій для лабораторної роботи [cite: 84, 86]
+  tracesSampleRate: 1.0,
+
+  // Важливо для звіту: вказуємо середовище [cite: 87, 88]
+  environment: "development",
+
+  // Дозволяє збирати IP та дані для аналізу (PII)
+  sendDefaultPii: true,
+});
+
+// Додаємо ідентифікацію користувача для Кроку 3
+Sentry.setUser({
+  id: "6-semester-student", // твій унікальний ID
+  email: "yulia@example.com", // твій email для звіту
+  username: "YuliaHabra"     // твоє ім'я/нікнейм
+});
 
 app.use(createPinia())
 
